5709bdc0a52b0dc62175e9c3b6883ec47e64ee9c
Aj Cabasis <ronron110912@gmail.com>
2025-08-11 10:48:47 +0800

    Update Temperature.java

--- Temperature.java
+++ Temperature.java
@@ -1,23 +1,27 @@
 import java.awt.*;
 import java.awt.event.*;
 
+//Implements The Window Frame for the UI
 public class Temperature extends Frame implements ActionListener{
+	//initializing components
 	Panel p1,p2,p3,p4,p6, p7,p8,p9,p10;
 	Label lp1,lp2,lp4,lp5,lp6,lp7;
 	Button b1,b2;
 	TextField t1,t2;
 	
+	//Constructor for temperature frame
 	public Temperature() {
 		setTitle("Temparature Converter");
 		setSize(380,270);
 		setLayout(new GridLayout(10,0));
 		setBackground(Color.LIGHT_GRAY);
 		
-		
+		//Celsius-Fahrenheit panel sets as label object panel
 		p1 = new Panel(new FlowLayout(FlowLayout.LEFT));
 		lp1 = new Label("Celsius to Fahrenheit");
 		p1.add(lp1);
 		
+		//Start temperature label and Conversion button set as the first panel, button listens for functionality
 		p2 = new Panel(new FlowLayout(FlowLayout.LEFT));
 		lp2 = new Label("Start Temp:");
 		t1 = new TextField(7);
@@ -25,7 +29,8 @@ public class Temperature extends Frame implements ActionListener{
 		b1.addActionListener(this);
 		p2.add(lp2);p2.add(t1); p2.add(b1);
 		
-	
+		
+		//Panel for spacing
 		p3 = new Panel(new BorderLayout());
 		lp4 = new Label();
 		lp4.setAlignment(Label.CENTER);
@@ -33,17 +38,20 @@ public class Temperature extends Frame implements ActionListener{
 		p4 = new Panel();
 		
 		
-
+		//Celsius-Fareheit panel sets as label object panel
 		p6 = new Panel(new FlowLayout(FlowLayout.LEFT));
 		lp5 = new Label("Fahrenheit to celsius");
 		p6.add(lp5);
 		
+		//Start temperature label and Conversion button set as the second panel, button listens for functionality
 		p7 = new Panel(new FlowLayout(FlowLayout.LEFT));
 		lp6= new Label("Start Temp:");
 		t2 = new TextField(7);
 		b2 = new Button("Convert");
 		b2.addActionListener(this);
 		p7.add(lp6);p7.add(t2); p7.add(b2);
+		
+		//Panel for spacing
 		p8 = new Panel(new BorderLayout());
 		lp7 = new Label();
 		lp7.setAlignment(Label.CENTER);
@@ -51,6 +59,8 @@ public class Temperature extends Frame implements ActionListener{
 		p9 = new Panel();
 		p10 = new Panel();
 		
+		
+		//All panels are added accordingly
 		add(p1);
 		add(p2);
 		add(p3);
@@ -65,11 +75,11 @@ public class Temperature extends Frame implements ActionListener{
 		
 		
 		
-		
 		setVisible(true);
 		setResizable(false);
 		
-
+		
+		//So that you can close the window using the X button 
 		addWindowListener(new WindowAdapter(){
 			public void windowClosing(WindowEvent e) {
 				dispose();
@@ -77,11 +87,15 @@ public class Temperature extends Frame implements ActionListener{
 			});
 	}
 	
+	//Initializes new Temperature Window 
 	public static void main(String[] args) {
 		new Temperature();
 	}
+	
+	//Button Functionality
 	public void actionPerformed(ActionEvent e) {
 		
+		//If it clicks the Celsius-Fahrenheit convert button, Calculates the result using the Fahrenheit Formula. Else It catches an error
 		if (e.getSource() == b1) {
 			try {
 				double c = Double.parseDouble(t1.getText());
@@ -95,6 +109,7 @@ public class Temperature extends Frame implements ActionListener{
 				
 			}
 	}
+		//If it clicks the Fahrenheit-Celsius convert button, Calculates the result using the Celsius Formula. Else It catches an error
 		if (e.getSource() == b2) {
 			try {
 				double F = Double.parseDouble(t2.getText());
